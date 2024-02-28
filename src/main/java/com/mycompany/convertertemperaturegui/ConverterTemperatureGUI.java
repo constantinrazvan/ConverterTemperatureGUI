e910a11fdb2135dd198a30684ed5ae37e231509c
constantinrazvan <razvanpana20@gmail.com>
2024-02-28 18:07:45 +0200

    removed some imports

--- src/main/java/com/mycompany/convertertemperaturegui/ConverterTemperatureGUI.java
+++ src/main/java/com/mycompany/convertertemperaturegui/ConverterTemperatureGUI.java
@@ -1,9 +1,7 @@
 package com.mycompany.convertertemperaturegui;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.awt.*;
 
 public class ConverterTemperatureGUI {
     
